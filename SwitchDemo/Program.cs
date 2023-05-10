7272c412e1d229587b4135d6c859488c5079df50
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-10 00:45:46 -0600

    code complete.

--- SwitchDemo/Program.cs
+++ SwitchDemo/Program.cs
@@ -2,4 +2,32 @@
 string firstName = "Mohammad";
 int age = 43;
 
-switch ()
+//switch (firstName.ToLower())
+//{
+//    case "mohammad":
+//        Console.WriteLine("Hello Mohammad.");
+//        break;
+//    //case "tom":
+//    case "tim" or "tom":
+//        Console.WriteLine("Hello Tim or Tom.");
+//        break;
+//    default:
+//        Console.WriteLine("I don't know you.");
+//        break;
+//}
+
+switch (age)
+{
+    case >= 0 and < 18:
+        Console.WriteLine("You are a child.");
+        break;
+    case >= 18 and < 66:
+        Console.WriteLine("You should have a job.");
+        break;
+    case >= 66:
+        Console.WriteLine("Retired or retiring?");
+        break;
+    default:
+        Console.WriteLine("Age not in expected range.");
+        break;  
+}
